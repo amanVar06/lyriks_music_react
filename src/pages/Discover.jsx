baa184277fa1edc3c5978ab806bfb4932afaec9f
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-11-30 10:49:02 +0530

    Completed discover component and song card component songs are playing successfully

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,13 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
   const { data: topCharts, isFetching, error } = useGetTopChartsQuery();
   console.log(topCharts);
 
@@ -31,7 +36,14 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {topCharts?.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            activeSong={activeSong}
+            isPlaying={isPlaying}
+            song={song}
+            i={i}
+            data={topCharts}
+          />
         ))}
       </div>
     </div>
