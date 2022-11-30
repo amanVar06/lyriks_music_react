5c482c85f2a5cd7f3fce9d622c51b4b8dcfb8fa4
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-11-30 22:41:33 +0530

    Fixed location issue in around you

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -6,11 +6,13 @@ import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 import { Error, Loader, SongCard } from "../components";
 
 const CountryTracks = () => {
-  const [country, setCountry] = useState("");
+  const [country, setCountry] = useState("IN");
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(
+    country || "IN"
+  );
 
   useEffect(() => {
     // if you will face issue like Failed to load resource: net::ERR_BLOCKED_BY_CLIENT then remove adBlocks from the browser
@@ -34,7 +36,7 @@ const CountryTracks = () => {
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
         Around You
-        <span className="font-black"> {country}</span>
+        <span className="font-black"> {country || "IN"}</span>
       </h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
