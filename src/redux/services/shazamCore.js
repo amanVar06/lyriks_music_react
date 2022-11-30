7d7bfc471e326127f2adfe5d7097f87c0018cd91
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-11-30 10:11:19 +0530

    successfully fetched data from shazam core api using redux toolkit and created a layout for discover component

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "a0cc82a5damsh2231dfdcc4f0503p1bee19jsnd01a38a195df"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
